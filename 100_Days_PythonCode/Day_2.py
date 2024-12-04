e6af3da47357da53ee6adf729dc6b0dae724be40
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2024-12-05 00:42:24 +0200

    Update Day_2.py

--- 100_Days_PythonCode/Day_2.py
+++ 100_Days_PythonCode/Day_2.py
@@ -21,3 +21,40 @@ print(False)
 
 street_name = "Abbey Road"
 print(street_name[4] + street_name[7]) #Will print "yo". It takes in counting the spaces as well
+
+len("12345")
+print(type("12345")) # will print the type of data
+print(type(123))
+print(type(12.45))
+print(type(True))
+
+print("123" + "345") #Will print the concatination
+int("123") #will convert the data from string to integer
+print(int("123") + int("345")) #will treat them as integers(numbers) and will print the sum of the two numbers
+
+
+# print("Number of letters in your name: " + len(input("Enter your name: ")))
+name = input("Enter your name: ")
+print("Number of letters in your name: " + str(len(name)))
+
+# using the / operator will return a float type result even if the operation is clean
+print(6 / 3)
+
+# using the ** raises the power ->2 to the power of 6
+print(2 ** 6)
+
+# THE RULE used to do math operations is PEMDAS = ( ) -> ** -> * / -> + -
+# PEMDAS = Parentheses -> Exponents -> Multiplication -> Division -> Addition -> Subtraction
+
+print(3 * 3 + 3 / 3 - 3)
+
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / (height ** 2)
+
+print(bmi)
+
+print(round(bmi))
