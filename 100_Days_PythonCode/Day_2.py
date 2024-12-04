59797dc077639b7c74a9508349e445a4601e768e
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2024-12-04 23:35:54 +0200

    Add files via upload

---
+++ 100_Days_PythonCode/Day_2.py
@@ -0,0 +1,23 @@
+#Data types
+# using the [] allows the user to select what character from that string to be printed.
+print("Hello"[4]) #will print "O"
+print("Hello"[-2]) #will print "l"
+
+# String
+print("123" + "345") #Will print 123345 "Concatenation
+
+# Integer = Whole number
+print(123 + 345) #Will print the sum
+
+# Large integers
+print(123_456_789) #For large numbers, in python it is used _ to allow the user to better visualize the numbers
+
+# Float = Floating Point numbers  (for decimal numbers)
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7]) #Will print "yo". It takes in counting the spaces as well
