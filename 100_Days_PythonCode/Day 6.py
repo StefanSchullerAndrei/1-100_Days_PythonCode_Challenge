132f3c880ac478a9cc104069c4b5881b5035e726
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2024-12-09 16:31:59 +0200

    Add files via upload

---
+++ 100_Days_PythonCode/Day 6.py
@@ -0,0 +1,56 @@
+# Defining functions
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# Calling the function
+my_function()
+
+
+# def jumping_fence():
+#     move()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+# number_of_hurdles = 0
+# while not at_goal():
+#     jumping_fence()
+#     number_of_hurdles += 1
+#     print(number_of_hurdles)
+
+# def jumping_fence():
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+#
+# def smart_robot():
+#     def wall_in_front():
+#         jumping_fence()
+#
+#     while front_is_clear() == True:
+#         move()
+#     while not front_is_clear():
+#         wall_in_front()
+#
+#
+# while not at_goal():
+#     smart_robot()
