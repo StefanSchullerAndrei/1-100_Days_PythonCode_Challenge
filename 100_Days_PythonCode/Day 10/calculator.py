cb6987b76e8f2764c16e50f84d908590c57587e6
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2024-12-17 17:52:55 +0200

    Add files via upload

---
+++ 100_Days_PythonCode/Day 10/calculator.py
@@ -0,0 +1,23 @@
+from art import logo
+def add(n1, n2):
+    return n1 + n2
+
+# TODO: Write out the other 3 functions - subtract, multiply and divide.
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+# TODO: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+# we DO NOT put parentheses because we do not want to trigger the functions
+}
+
