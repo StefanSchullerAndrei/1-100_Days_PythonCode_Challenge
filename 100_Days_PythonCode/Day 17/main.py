2406328d55f5c9d01969fb3d98f5ef7b6620b1c3
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2025-01-20 14:53:32 +0200

    Update main.py

--- 100_Days_PythonCode/Day 17/main.py
+++ 100_Days_PythonCode/Day 17/main.py
@@ -12,5 +12,8 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 quiz.next_question()
 
-# while quiz.still_have_questions():
-#     quiz.next_question()
+while quiz.still_have_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}!")
