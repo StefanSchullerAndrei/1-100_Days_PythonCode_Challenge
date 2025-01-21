f0df64a337ea9bedb4faf60f31ed586b9e70cf98
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2025-01-21 16:32:13 +0200

    Update main.py

--- 100_Days_PythonCode/Day 18/main.py
+++ 100_Days_PythonCode/Day 18/main.py
@@ -4,9 +4,14 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+for _ in range(100):
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+
+import turtle as t # creating new object with an allias name
+tim = t.Turtle()
 
 
 
