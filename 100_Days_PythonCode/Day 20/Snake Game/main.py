20e41fc3fd3d5c8671c74197222655fdcac90a94
Stefan-Schuller Andrei <andrei_schuller2000@yahoo.com>
2025-01-28 16:13:52 +0200

    Update main.py

--- 100_Days_PythonCode/Day 20/Snake Game/main.py
+++ 100_Days_PythonCode/Day 20/Snake Game/main.py
@@ -1,9 +1,10 @@
 from turtle import Screen, Turtle
-
+import time
 screen = Screen()
 screen.setup(width=600, height=600)   # setting the window size
 screen.bgcolor("black") # changing the background of the console
 screen.title("My Snake Game")
+screen.tracer(0)
 
 # Challenge: Create 3 turtles and position them next to each other.
 #Each turtle should be a white square (default size: 20x20)
@@ -11,11 +12,14 @@ screen.title("My Snake Game")
 # ---------------- Method 1 -----------------------
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
 # ----------------- Method 2 ---------------------
 # segment_1 = Turtle("square")
@@ -28,6 +32,22 @@ for position in starting_positions:
 # segment_3 = Turtle("square")
 # segment_3.color("white")
 # segment_3.goto(x=-40, y=0)
+
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):   # start = is the number that we're going to start the range from,
+            new_x = segments[seg_num - 1].xcor()        # stop = is where the range is going to end
+            new_y = segments[seg_num - 1].ycor()            # step = is how we're going to get from the start to the stop
+            segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
+
 
 
 screen.exitonclick()
